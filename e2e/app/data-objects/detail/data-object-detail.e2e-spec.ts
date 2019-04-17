6ac199e295bacd56fe6f5e2a184a8644ed4a0246
K28088 <K28088>
2019-04-17 12:27:48 -0400

    DM-11339 : UI-View Retention Information for BData.
    -Addressing the Comment.

--- e2e/app/data-objects/detail/data-object-detail.e2e-spec.ts
+++ e2e/app/data-objects/detail/data-object-detail.e2e-spec.ts
@@ -100,14 +100,10 @@ describe('data-objects detail', () => {
 
     it('Details info: BData details page with Retention Expiration Date', async () => {
         await page.navigateTo(baseDetail.replaceUrlParams(data.bdataWithRetentionExpirationDate, null, 0));
-        var date = new Date('2019-04-12T18:03:03.399Z');
-        var day = (date.getDate()<10?'0':'')+ date.getDate();
-        var month = ((date.getMonth() + 1)<10?'0':'')+ (date.getMonth() + 1);
-        var year = date.getFullYear() + "";
-        var hour = (date.getHours()<10?'0':'')+ date.getHours();
-        var minutes = (date.getMinutes()<10?'0':'')+ date.getMinutes();
-        var seconds = (date.getSeconds()<10?'0':'')+ date.getSeconds();
-        var expectedExpirationDate=year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
+        var dateFormat = require('dateformat');
+        var retentionExpirationDate = new Date('2019-04-12T18:03:03.399z');
+        var expectedExpirationDate = dateFormat (retentionExpirationDate,"yyyy-mm-dd HH:MM:ss");
+        console.log(expectedExpirationDate);
         const pageTitle = browser.getTitle();
         expect(await pageTitle).toContain(conf.docTitlePrefix + ' - Data Object');
         expect(await page.detailsHeaderLabel.getText()).toBe('Details');
