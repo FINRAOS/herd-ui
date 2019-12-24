2deaab509de6d56e4a9933dc8e1eaf998ae7c595
Maggie Wang <maggie.wang@finra.org>
2019-12-23 20:26:23 -0500

    CATDM-358: TestFactoryCQ - Implement CATDM UDC smokeTest with FIP login
    - increase jasmine timeout to 5 minutes

--- e2e/app/smoke/smoke.e2e-spec.ts
+++ e2e/app/smoke/smoke.e2e-spec.ts
@@ -41,8 +41,8 @@ describe('Smoke Tests', () => {
   const categoryPage = new CategoryPage();
   const dataObjectList = new DataObjectListPage();
   const dataObjectDetailPage = new DataObjectDetailPage();
-  // increase jasmine timeout to 3 minutes
-  jasmine.DEFAULT_TIMEOUT_INTERVAL  = 180000;
+  // increase jasmine timeout to 5 minutes
+  jasmine.DEFAULT_TIMEOUT_INTERVAL  = 300000;
 
   it('should should display proper headings and category data', async () => {
 
